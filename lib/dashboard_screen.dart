c13ccd261ec33b583b21e683536ecdc01842378f
Pushparaj Manickam <pushparaj.manickam@gavstech.com>
2024-02-18 11:21:09 +0530

    creating new file with routing

---
+++ lib/dashboard_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0,),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  context.go("/textfieldwidget");
+                },
+                child: Text(
+                  'Text Field Widget',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
